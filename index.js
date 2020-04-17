7d4d77dfdec4bf8cf0683efa245dab4fa2114661
Thanh Pham Kieu <thanhpk@live.com>
2020-04-18 00:31:38 +0700

    add index

---
+++ index.js
@@ -0,0 +1,10 @@
+var sleep = require('./sleep.js')
+var loop = require('./loop.js')
+var map = require('./map.js')
+var batch = require('./batch.js')
+
+module.exports = {
+	loop: loop,
+	map: map,
+	batch: batch,
+}
